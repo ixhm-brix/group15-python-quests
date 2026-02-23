1d078def37d89c6f9a70499931c98b3e0a321653
ixhm-brix <f.ishimwe1@alustudent.com>
2026-02-24 00:49:51 +0200

    q4

---
+++ quest_04-the_town_crier.py
@@ -0,0 +1,6 @@
+#Create variables for a city name, the current year, and your name. Print a message like: 
+# "Welcome to [City Name]! The year is [Year], and our newest resident is [Your Name]."
+CityName = "Kigali"
+CurrentYear = 2026
+Name = "fab"
+print (f"Welcome to {CityName}! The year is {CurrentYear}, and our newest resident is {Name}")
